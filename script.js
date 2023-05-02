041fe073a3b348d61df5f1783b552246c271d7a8
Cory Rewis <Crewis1991@gmail.com>
2023-05-02 18:53:03 -0500

    complete task

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+var i = 0;
+var txt = 'its just a basic website';
+var speed = 50; 
+
+function type() {
+  if (i < txt.length) {
+    document.getElementById("write").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(type, speed);
+  }
+}
